560ea6aa1342f0373f91c15e1a01f7f6cf69bb05
Seungwoo Paek <swpaek77@gmail.com>
2023-05-31 17:53:24 +0900

    feat: add zustand persist

--- src/stores/ResourceStore.ts
+++ src/stores/ResourceStore.ts
@@ -1,6 +1,7 @@
 import { Moment } from "moment";
 import { toast } from "react-toastify";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 import { getTypeResource } from "@/helpers/convert";
@@ -32,42 +33,48 @@ export const initResourceData = {
 };
 
 export const useResourceStore = create(
-  immer<ResourceState>(set => ({
-    ...initResourceData,
-    addResource: resource => {
-      toast.success(`[${resource.name}] ${getTypeResource(resource.type)} 추가에 성공했습니다!`);
-      set(state => {
-        state.resourceList.unshift(resource);
-      });
-    },
-    removeResource: resource => {
-      set(state => {
-        state.resourceList = state.resourceList.filter(res => res.id !== resource.id);
-        if (state.selectedResource?.id === resource.id) {
+  persist(
+    immer<ResourceState>(set => ({
+      ...initResourceData,
+      addResource: resource => {
+        toast.success(`[${resource.name}] ${getTypeResource(resource.type)} 추가에 성공했습니다!`);
+        set(state => {
+          state.resourceList.unshift(resource);
+        });
+      },
+      removeResource: resource => {
+        set(state => {
+          state.resourceList = state.resourceList.filter(res => res.id !== resource.id);
+          if (state.selectedResource?.id === resource.id) {
+            state.selectedResource = undefined;
+          }
+        });
+      },
+      editResourceName: (resource, name) => {
+        set(state => {
+          const curResource = state.resourceList.find(res => res.id === resource.id);
+          if (curResource?.name) {
+            curResource.name = name;
+          }
+          if (state.selectedResource?.id === resource.id) {
+            state.selectedResource.name = name;
+          }
+        });
+      },
+      selectResource: resource => {
+        set(state => {
+          state.selectedResource = resource;
+        });
+      },
+      unselectResource: () => {
+        set(state => {
           state.selectedResource = undefined;
-        }
-      });
-    },
-    editResourceName: (resource, name) => {
-      set(state => {
-        const curResource = state.resourceList.find(res => res.id === resource.id);
-        if (curResource?.name) {
-          curResource.name = name;
-        }
-        if (state.selectedResource?.id === resource.id) {
-          state.selectedResource.name = name;
-        }
-      });
-    },
-    selectResource: resource => {
-      set(state => {
-        state.selectedResource = resource;
-      });
-    },
-    unselectResource: () => {
-      set(state => {
-        state.selectedResource = undefined;
-      });
-    },
-  }))
+        });
+      },
+    })),
+    {
+      name: "29cm-cart-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
 );
